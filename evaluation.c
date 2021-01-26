03ea0899a9db2de73e3b2f7d7e7672b4fb8c2396
szy1840 <szy1840@gmail.com>
2021-01-26 20:55:38 +0800

    evaluation

--- evaluation.c
+++ evaluation.c
@@ -32,6 +32,7 @@ enum {LVAL_ERR,LVAL_NUM,LVAL_SYM,LVAL_SEXPR};
 
 /* prototypes */
 void lval_print(lval* v);
+lval* lval_eval(lval* v);
 
 /* construct a pointer to a new number type lval */
 lval* lval_num(long x){
@@ -163,6 +164,97 @@ void lval_println(lval* v){
     lval_print(v);
     putchar('\n');
 }
+/* lval_pop takes an element from the given list and pop it,
+while lval_take also delete the list and leave the element only */
+lval* lval_pop(lval* v, int i){
+    /* x gets the content of v->cell[i](though it is an address),
+    so realloc won't make the content of x invalid */
+    lval* x=v->cell[i];
+
+    /* shift the memory */
+    memmove(&v->cell[i],&v->cell[i+1],sizeof(lval*)*(v->count-i-1));
+    v->count--;
+    v->cell=realloc(v->cell,sizeof(lval*)*(v->count));
+    return x;
+}
+lval* lval_take(lval* v, int i){
+    lval* x=lval_pop(v,i);
+    lval_del(v);
+    return x;
+}
+/* evaluation */
+lval* builtin_op(lval* a,char* op){
+    for (int i = 0; i < a->count; i++)
+    {
+        if(a->cell[i]->type!=LVAL_NUM){
+            lval_del(a);
+            return lval_err("Cannot operate on non-number!");
+        }
+    }
+    
+    lval* x=lval_pop(a,0);
+    /*  it's zero because we used pop to trim the first elem(symbol) in the eval func*/
+    if((strcmp(op,"-")==0) && a->count==0) x->num=-(x->num);
+
+    while(a->count>0){
+        lval* y=lval_pop(a,0);
+
+        if(strcmp(op,"+")==0) x->num += y->num;
+        if(strcmp(op,"-")==0) x->num -= y->num;
+        if(strcmp(op,"*")==0) x->num *= y->num;
+        if(strcmp(op,"/")==0) {
+            if(y->num==0){
+                lval_del(x);
+                lval_del(y);
+                x=lval_err("Division by zero!");
+                break;
+            }
+            x->num /= y->num;
+        }
+        lval_del(y);
+    }
+    lval_del(a);
+    /* remember x is popped so we can safely del a */
+    return x;
+}
+lval* lval_eval_sexpr(lval* v){
+    for (int i = 0; i < v->count; i++)
+    {
+        v->cell[i]=lval_eval(v->cell[i]);
+    }
+    /* error checking */
+    for (int i = 0; i < v->count; i++)
+    {
+        if(v->cell[i]->type==LVAL_ERR){
+            return lval_take(v,i);
+        }
+    }
+    /* empty expression */
+    if(v->count==0) return v;
+    /* single expression */
+    if(v->count==1) return lval_take(v,0);
+
+    /* ensure the first element is symbol */
+    lval* f=lval_pop(v,0);
+    if(f->type!=LVAL_SYM){
+        lval_del(f);
+        lval_del(v);
+        return lval_err("S-expression should start with symbol!");
+    }
+
+    lval* result=builtin_op(v,f->sym);
+    lval_del(f);
+    return result;
+}
+
+lval* lval_eval(lval* v){
+    /* evaluate Sexpressions */
+    if(v->type==LVAL_SEXPR){
+        return lval_eval_sexpr(v);
+    }
+
+    return v;
+}
 
 int main(int argc, char **argv)
 {
@@ -193,7 +285,7 @@ int main(int argc, char **argv)
         /* attempt to parse the user input */
         mpc_result_t r;
         if(mpc_parse("<stdin>",input,Lispy,&r)){
-            lval* x=lval_read(r.output);
+            lval* x=lval_eval(lval_read(r.output));
             lval_println(x);
             lval_del(x);
         }else{
